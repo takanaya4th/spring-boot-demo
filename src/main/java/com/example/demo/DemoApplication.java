cacf286d5263aee79871ca3d8dd0e04b2d6c8de5
takanaya4th <takanaya.4th@gmail.com>
2020-08-07 23:19:18 +0900

    Modify tab to space

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	/**
-	 * Javadocテキストの複数行がここに 書き込まれます 通常のラップ...
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    /**
+     * Javadocテキストの複数行がここに 書き込まれます 通常のラップ...
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
